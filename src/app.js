5eedc37d27305933fb3a6649cb82fb981937d674
anurgazin <54969694+anurgazin@users.noreply.github.com>
2022-01-28 22:11:46 -0500

    HTTP Responses was rewrited with using response.js functions

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const compression = require('compression');
 const passport = require('passport');
 const authorization = require('./authorization');
 
+const { createErrorResponse } = require('./response');
+
 // Version from our package.json file
 //const { version } = require('../package.json');
 
@@ -40,13 +42,15 @@ app.use('/', require('./routes'));
 
 // Add 404 middleware to handle any requests for resources that can't be found can't be found
 app.use((req, res) => {
-  res.status(404).json({
-    status: 'error',
-    error: {
-      message: 'not found',
-      code: 404,
-    },
-  });
+  res.status(404).json(
+    createErrorResponse({
+      status: 'error',
+      error: {
+        message: 'not found',
+        code: 404,
+      },
+    })
+  );
 });
 
 // Add error-handling middleware to deal with anything else
@@ -62,13 +66,15 @@ app.use((err, req, res, next) => {
     logger.error({ err }, `Error processing request`);
   }
 
-  res.status(status).json({
-    status: 'error',
-    error: {
-      message,
-      code: status,
-    },
-  });
+  res.status(status).json(
+    createErrorResponse({
+      status: 'error',
+      error: {
+        message,
+        code: status,
+      },
+    })
+  );
 });
 
 // Export our `app` so we can access it in server.js
