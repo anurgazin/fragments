b0c88c6f6ea86ce0e0f1a23663a3c88b64577e46
anurgazin <54969694+anurgazin@users.noreply.github.com>
2022-01-28 21:15:32 -0500

    Unit Tests were Created

---
+++ src/response.js
@@ -0,0 +1,43 @@
+// src/response.js
+
+const { status } = require('express/lib/response');
+
+/**
+ * A successful response looks like:
+ *
+ * {
+ *   "status": "ok",
+ *   ...
+ * }
+ */
+module.exports.createSuccessResponse = function (data) {
+  return {
+    status: 'ok',
+    // TODO ...
+    // Use the spread operator to clone `data` into our object, see:
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax#spread_in_object_literals
+    ...data,
+  };
+};
+
+/**
+ * An error response looks like:
+ *
+ * {
+ *   "status": "error",
+ *   "error": {
+ *     "code": 400,
+ *     "message": "invalid request, missing ...",
+ *   }
+ * }
+ */
+module.exports.createErrorResponse = function (code, message) {
+  // TODO ...
+  return {
+    error: {
+      code: code,
+      message: message,
+    },
+    status: 'error',
+  };
+};
