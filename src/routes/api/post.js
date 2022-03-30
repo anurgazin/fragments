50806d3ad6a924c0f55f0615387e856cd8dd1522
anurgazin <54969694+anurgazin@users.noreply.github.com>
2022-03-29 21:38:21 -0400

    Integration Testing with HURL | AWS Mocking

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -1,20 +1,24 @@
 const { createSuccessResponse, createErrorResponse } = require('../../response');
 const { Fragment } = require('../../model/fragment');
 const hash = require('../../hash');
-const apiURL = process.env.API_URL;
+const apiURL = 'http://localhost:8080/v1';
 module.exports = async (req, res) => {
-  try {
-    const fragment = new Fragment({ ownerId: hash(req.user), type: req.get('Content-Type') });
-    await fragment.setData(req.body);
-    res.setHeader('Content-type', fragment.type);
-    res.setHeader('Location', apiURL + 'fragments' + fragment.id);
-    res.status(201).json(
-      createSuccessResponse({
-        status: 'ok',
-        fragments: fragment,
-      })
-    );
-  } catch (error) {
-    res.status(415).json(createErrorResponse(415, error));
+  if (Fragment.isSupportedType(req.get('Content-Type'))) {
+    try {
+      const fragment = new Fragment({ ownerId: hash(req.user), type: req.get('Content-Type') });
+      await fragment.setData(req.body);
+      res.setHeader('Content-type', fragment.type);
+      res.setHeader('Location', apiURL + '/fragments/' + fragment.id);
+      res.status(201).json(
+        createSuccessResponse({
+          status: 'ok',
+          fragment: fragment,
+        })
+      );
+    } catch (error) {
+      res.status(401).json(createErrorResponse(401, error));
+    }
+  } else {
+    res.status(415).json(createErrorResponse(415, 'Unsupported Content Type'));
   }
 };
