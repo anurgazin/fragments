fcb6c094191b4ec6884083207776e0cd648cf11d
anurgazin <54969694+anurgazin@users.noreply.github.com>
2022-04-12 09:24:29 -0400

    Support of DynamoDB is added/New Integration Test is added

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -1,14 +1,14 @@
 const { createSuccessResponse, createErrorResponse } = require('../../response');
 const { Fragment } = require('../../model/fragment');
 const hash = require('../../hash');
-const apiURL = 'http://localhost:8080/v1';
+const apiURL = process.env.API_URL;
 module.exports = async (req, res) => {
   if (Fragment.isSupportedType(req.get('Content-Type'))) {
     try {
       const fragment = new Fragment({ ownerId: hash(req.user), type: req.get('Content-Type') });
       await fragment.setData(req.body);
       res.setHeader('Content-type', fragment.type);
-      res.setHeader('Location', apiURL + '/fragments/' + fragment.id);
+      res.setHeader('Location', apiURL + '/v1/fragments/' + fragment.id);
       res.status(201).json(
         createSuccessResponse({
           status: 'ok',
